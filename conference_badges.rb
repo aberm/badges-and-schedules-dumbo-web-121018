b34322e344fdb992a023b635c79fbedbdc8fdb80
Menachem Aber <abermendel@gmail.com>
2018-11-23 02:03:01 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array1 = []
+  array.each do |name|
+    array1.push(badge_maker(name))
+  end
+  array1
+end
+
